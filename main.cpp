4495cea560a32078e2e4011add6f682ec1f69447
Permyakov Ivan <62116118+inffe@users.noreply.github.com>
2023-12-29 15:19:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <iostream>
 
 std::vector<std::complex<double>> matrixMul(int size, std::complex<double> matrixA[], std::complex<double> matrixB[]) {
     std::vector<std::complex<double>> matrixC;
@@ -46,7 +47,7 @@ int main()
     matrixH[1] = {2, 4};
     matrixH[4] = {1, 0};
     matrixH[5] = {3, 1};
-    matrixH[3] = {2,4};
+    matrixH[3] = {2, 4};
     matrixH[7] = {3, 1};
     matrixH[8] = {2, 0};
 
@@ -54,7 +55,7 @@ int main()
     matrixL[1] = {4, 1};
     matrixL[4] = {2, 0};
     matrixL[5] = {2, 3};
-    matrixL[3] = {4,-1};
+    matrixL[3] = {4, -1};
     matrixL[7] = {2, -3};
     matrixL[8] = {4, 0};
 
@@ -99,7 +100,6 @@ int main()
 // ну типа считаем h и l
 
     std::complex<double> h[9] = {};
-
     for (int i = 1; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
             h[i] += matrixH[j]*fBasis[i * 9 + j];
@@ -108,54 +108,123 @@ int main()
 
     // сократить, зачем тут считать только нужные
     std::complex<double> l[9] = {};
+    std::complex<double> lPodvox[9] = {};
     for (int i = 1; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
             l[i] += matrixL[j]*fBasis[i * 9 + j];
         }
     }
 
+    for (int i = 1; i < 9; ++i) {
+        lPodvox[i] = std::conj(l[i]);
+    }
+
 // ну типа считаем Fmns и Zmns
 
-    std::complex<double> Fmns[8][8][8] = {0};
-    std::complex<double> Zmns[8][8][8] = {0};
-    std::complex<double> filler = {0 , -1};
+    int countFmns = 0;
+    int countZmns = 0;
+
+    std::complex<double> Fmns[9][9][9] = {0};
+    std::complex<double> Zmns[9][9][9] = {0};
+    std::complex<double> filler = {0 , 1};
     for (int i = 1; i < 9; ++i) {
         for (int j = 1; j < 9; ++j) {
             for (int k = 1; k < 9; ++k) {
-                std::vector<std::complex<double>> vector = {};
+                std::vector<std::complex<double>> vectorComm = {};
                 std::vector<std::complex<double>> vector1 = {};
                 std::vector<std::complex<double>> vector2 = {};
+                std::vector<std::complex<double>> vectorResult = {};
 
                 std::complex<double> Fm[9] = {};
                 std::complex<double> Fn[9] = {};
                 std::complex<double> Fs[9] = {};
+
+                std::complex<double> comm[9] = {};
+                std::complex<double> result[9] = {};
+
                 for (int o = 0; o < 9; ++o) {
                     Fm[o] = fBasis[i * 9 + o];
                     Fn[o] = fBasis[k * 9 + o];
                     Fs[o] = fBasis[j * 9 + o];
                 }
 
+                // расчет коммутатора
                 vector1 = matrixMul(3, Fm, Fn);
-
                 vector2 = matrixMul(3, Fn, Fm);
-
-                std::transform(vector1.begin(), vector1.end(), vector2.begin(), std::back_inserter(vector), [&](std::complex<double> one, std::complex<double> two){
+                std::transform(vector1.begin(), vector1.end(), vector2.begin(), std::back_inserter(vectorComm), [&](std::complex<double> one, std::complex<double> two){
                     return one - two;
                 });
 
-                vector1 = matrixMul(3, Fs, vector);
+                for (int p = 0; p < 9; ++p) {
+                    comm[p] = vectorComm[p];
+                }
 
-                Fmns[k][j][i] = filler * trace(vector);
+                vectorResult = matrixMul(3, Fs, comm);
+
+                for (int p = 0; p < 9; ++p) {
+                    result[p] = vectorResult[p];
+                }
+
+                Fmns[i][j][k] = -filler * trace(result);
+                if (arg(Fmns[i][j][k]) != 0 || imag(Fmns[i][j][k]) != 0) {
+                    ++countFmns;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < 9; ++i) {
+        for (int j = 1; j < 9; ++j) {
+            for (int k = 1; k < 9; ++k) {
+                std::vector<std::complex<double>> vectorComm = {};
+                std::vector<std::complex<double>> vector1 = {};
+                std::vector<std::complex<double>> vector2 = {};
+                std::vector<std::complex<double>> vectorResult = {};
+
+                std::complex<double> Fm[9] = {};
+                std::complex<double> Fn[9] = {};
+                std::complex<double> Fs[9] = {};
+
+                std::complex<double> comm[9] = {};
+                std::complex<double> result[9] = {};
+
+                for (int o = 0; o < 9; ++o) {
+                    Fm[o] = fBasis[i * 9 + o];
+                    Fn[o] = fBasis[k * 9 + o];
+                    Fs[o] = fBasis[j * 9 + o];
+                }
+
+                // расчет коммутатора
+                vector1 = matrixMul(3, Fm, Fn);
+                vector2 = matrixMul(3, Fn, Fm);
+                std::transform(vector1.begin(), vector1.end(), vector2.begin(), std::back_inserter(vectorComm), [&](std::complex<double> one, std::complex<double> two){
+                    return one + two;
+                });
+
+                for (int p = 0; p < 9; ++p) {
+                    comm[p] = vectorComm[p];
+                }
+
+                vectorResult = matrixMul(3, Fs, comm);
+
+                for (int p = 0; p < 9; ++p) {
+                    result[p] = vectorResult[p];
+                }
+
+                Zmns[i][j][k] = Fmns[i][j][k] + filler * trace(result);
+                if (arg(Zmns[i][j][k]) != 0 || imag(Zmns[i][j][k]) != 0) {
+                    ++countZmns;
+                }
             }
         }
     }
 
     // part C
-    std::complex<double> matrixQ[8][8] = {0};
+    std::complex<double> matrixQ[9][9] = {};
     for (int i = 1; i < 9; ++i){
         if (arg(h[i]) != 0 || imag(h[i]) != 0) {
             for (int m = 1; m < 9; ++m) {
-                for (int n = 1; n < 8; ++n) {
+                for (int n = 1; n < 9; ++n) {
                     matrixQ[m][n] += h[i]*Fmns[i][m][n]; //проверить порядок индексов
                 }
             }
@@ -163,12 +232,12 @@ int main()
     }
 
     // part D
-    std::complex<double> matrixK[8] = {};
-    std::complex<double> fillerMatrixK[8][8] = {};
+    std::complex<double> matrixK[9] = {};
+    std::complex<double> fillerMatrixK[9][9] = {};
     for (int i = 1; i < 9; ++i) {
         if (arg(l[i]) != 0 || imag(l[i]) != 0) {
             for (int m = 1; m < 9; ++m) {
-                for (int n = 1; n < 8; ++n) {
+                for (int n = 1; n < 9; ++n) {
                     fillerMatrixK[m][n] += l[i]*Fmns[i][m][n]; //проверить порядок индексов
                 }
             }
@@ -176,8 +245,10 @@ int main()
     }
     for (int i = 1; i < 9; ++i) {
         for (int m = 1; m < 9; ++m) {
-            for (int n = 1; n < 9; ++n) {
-                fillerMatrixK[m][n] = l[i]*fillerMatrixK[m][n];
+            if (arg(l[m]) != 0 || imag(l[m]) != 0) {
+                fillerMatrixK[i][m] = lPodvox[m]*fillerMatrixK[m][i];
+            } else {
+                fillerMatrixK[i][m] = {0};
             }
         }
     }
@@ -188,14 +259,46 @@ int main()
     }
 
     //part E
-    std::complex<double> fillerMatrixEone[8][8] = {0};
+    std::complex<double> fillerMatrixEone[9][9] = {0};
     for (int i = 1; i < 9; ++i){
         if (arg(l[i]) != 0 || imag(l[i]) != 0) {
             for (int m = 1; m < 9; ++m) {
-                for (int n = 1; n < 8; ++n) {
+                for (int n = 1; n < 9; ++n) {
                     fillerMatrixEone[m][n] += l[i]*Zmns[i][m][n]; //проверить порядок индексов
                 }
             }
         }
     }
+
+    std::complex<double> fillerMatrixEtwo[9][9] = {0};
+    for (int i = 1; i < 9; ++i){
+        if (arg(l[i]) != 0 || imag(l[i]) != 0) {
+            for (int m = 1; m < 9; ++m) {
+                for (int n = 1; n < 9; ++n) {
+                    fillerMatrixEtwo[m][n] += lPodvox[i]*Fmns[i][m][n]; //проверить порядок индексов
+                }
+            }
+        }
+    }
+
+
+    std::complex<double> matrixR[9][9] = {};
+    for (int i = 1; i < 9; ++i) {
+        for (int j = 1; j < 9; ++j) {
+            std::complex<double> qqq = {};
+            for (int u = 1; u < 9; ++u) {
+                qqq += fillerMatrixEone[i][u] + fillerMatrixEtwo[j][u];
+            }
+            matrixR[i][j] = qqq;
+        }
+    }
+
+    for (int i = 1; i < 9; ++i) {
+        for (int j = 1; j < 9; ++j) {
+            std::cout << matrixR[i][j].real() << " " << i << " " << j << std::endl;
+        }
+    }
+
+    int qweqweqwe = 1;
+
 }
